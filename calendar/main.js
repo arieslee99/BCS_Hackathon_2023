271a2b3abdd5359e6fc900ae2ff38442e3bf333e
Janie H <janieholzmann@hotmail.com>
2023-03-25 13:16:34 -0700

    changes

--- calendar/main.js
+++ calendar/main.js
@@ -1,4 +1,6 @@
-const events = [];
+const eventNames = ["dentist", "doctor"];
+const eventDays = [12, 22];
+//const events = [1,2,3];
 
 // display date and time
 function displayDateAndTime() {
@@ -10,6 +12,14 @@ function displayDateAndTime() {
 
 // add event to list of events
 function addEvent() {
+
+    // buttons = document.getElementsByTagName("button"); // list of all buttons
+    // for (var i = 0; i <= buttons.length; i += 1) {
+    //     buttons[i].onclick = function(e) {
+    //         alert(this.id);
+    //     };
+    // }
+
     var eventTitle = document.getElementById('eventTitleInput').value;
     var eventMonth = document.getElementById('eventMonthInput').value;
     var eventDay = document.getElementById('eventDayInput').value;
@@ -18,19 +28,22 @@ function addEvent() {
     const event = {title:eventTitle, month:eventMonth, day:eventDay};
 
     events.push(event);
-    return "New Event Added: " + eventTitle + " on" + eventMonth + " " + eventDay;
+    return eventTitle;
+    //return "New Event Added: " + eventTitle + " on" + eventMonth + " " + eventDay;
 }
 
 
 // display all events in list as a string
 function displayEvents() {
     var eventsAsString = "";
-    for (var i = 0; i < events.length; i++) {
-        var e = events[i];
-        var stringToAdd = "Title: " + e.title + "When: " + e.month + " " + e.day + ", at " + e.time + "\n";
-        eventsAsString.concat(stringToAdd);
+    // for (var i = 0; i < eventNames.length; i++) {
+    //     var e = events[i];
+    //     var stringToAdd = "Title: " + e.title + "When: " + e.month + " " + e.day + ", at " + e.time + "\n";
+    //     eventsAsString.concat(stringToAdd);
 
-    }
+    // }
+    eventsAsString.
+    eventsAsString.concat("hi");
     return eventsAsString;
 }
 
@@ -44,7 +57,7 @@ function eventPassed(event) {
 
 // removes old events from list of events
 function removeOldEvents() {
-    for (var i = 0; i < events.length; i++) {
+    for (var i = 0; i < eventNames.length; i++) {
         var e = events[i];
         if (eventPassed(e)) {
             events.splice(i, 1);
@@ -52,8 +65,17 @@ function removeOldEvents() {
     }
 }
 
-function showDaySummary() {
-    //
+function showDaySummary(day) {
+    var eventsOnDay = "";
+    var stringToAdd = "";
+    for (var i = 0; i < events.length; i++) {
+        if (events[i].day === day) {
+            stringToAdd = "Event: " + eventTitle + " on" + eventMonth + " " + eventDay; 
+            eventsOnDay.push(stringToAdd);
+        }
+    }
+    return eventsOnDay;
 }
+
 
 
