7b6f07e090cbff3ce5d56e97491fdb7209f28465
Janie H <janieholzmann@hotmail.com>
2023-03-25 11:52:57 -0700

    removeEvent

--- calendar/main.js
+++ calendar/main.js
@@ -5,56 +5,55 @@ const events = [];
 // display date and time
 function displayDateAndTime() {
     var today = new Date();
-    var day = today.getDay();
-    var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
-    var hour = today.getHours();
-    var minute = today.getMinutes();
-    var second = today.getSeconds();
-    var prepand = (hour >= 12) ? " PM " : " AM ";
-    hour = (hour >= 12) ? hour - 12 : hour;
-    if (hour === 0 && prepand === ' PM ') {
-        if (minute === 0 && second === 0) {
-            hour = 12;
-            prepand = ' Noon';
-        }
-        hour = 12;
-        prepand = ' PM';
-    }
-    if (hour === 0 && prepand === ' AM ') {
-        if (minute === 0 && second === 0) {
-            hour = 12;
-            prepand = ' Midnight';
-        } else {
-            hour = 12;
-            prepand = ' AM';
-        }
-    }
-    dateString = "Today is : " + daylist[day] + ". " + "Current Time : " + hour + " : " + minute + " : " + second + prepand;
-    return dateString;
+    var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+    var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return dayList[today.getDay()] + ", " + monthList[today.getMonth()] + " " + today.getUTCDate() + ", " + today.getHours() + ":" + today.getMinutes();
 }
 
 // add event to list of events
 function addEvent() {
     var eventTitle = document.getElementById('eventTitle').value;
-    var eventDate = document.getElementById('eventDate').value;
+    var eventMonth = document.getElementById('eventMonth').value;
+    var eventDay = document.getElementById('eventDay').value;
     var eventTime = document.getElementById('eventTime').value;
 
-    const event = {title:eventTitle, date:eventDate, time:eventTime};
+    const event = {title:eventTitle, month:eventMonth, day:eventDay, time:eventTime};
 
     events.push(event);
     return "New Event Added: " + eventTitle + ", at " + eventTime + " on" + eventDate;
 }
 
+
 // display all events in list as a string
 function displayEvents() {
     var eventsAsString = "";
-    for (var i = 0; events.length; i++) {
-        var e = events(i);
-        var stringToAdd = "Title: " + e.title + "When: " + e.date + ", at " + e.time + "\n";
+    for (var i = 0; i < events.length; i++) {
+        var e = events[i];
+        var stringToAdd = "Title: " + e.title + "When: " + e.month + " " + e.day + ", at " + e.time + "\n";
         eventsAsString.concat(stringToAdd);
 
     }
     return eventsAsString;
 }
 
+// return true if event has passed
+function eventPassed(event) {
+    today = new Date();
+    eventDay = new Date(2023, event.month, event.day);
+
+    return today.getTime() >= eventDay.getTime()
+}
+
+function removeOldEvents() {
+    for (var i = 0; i < events.length; i++) {
+        var e = events[i];
+        if (eventPassed(e)) {
+            events.splice(i, 1);
+        }
+    }
+
+}
+
+document.getElementById('addEventButton').addEventListener('click', addEvent);
+document.getElementById('displayAllUpcomingEvents').innerText = displayEvents();
 
